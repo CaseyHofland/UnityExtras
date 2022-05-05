f48a27dbfc5a7ddd975c05f27fb270e997e01beb
Casey <hofland.casey@gmail.com>
2022-05-05 22:08:17 +0200

    Add a Continuous Interaction
    The continuous interaction can be used with the Input System as an interaction that will continuously trigger performed. This makes working with InputActions in code much easier.

---
+++ Runtime/Input/Interactions/ContinuousInteraction.cs
@@ -0,0 +1,45 @@
+﻿#nullable enable
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace UnityExtras.Input
+{
+#if UNITY_EDITOR
+    [UnityEditor.InitializeOnLoad]
+#endif
+    public class ContinuousInteraction : IInputInteraction
+    {
+        public float activationTime { get; private set; }
+        private float lastDeltaTime;
+
+        static ContinuousInteraction()
+        {
+            InputSystem.RegisterInteraction<ContinuousInteraction>();
+        }
+
+        public void Process(ref InputInteractionContext context)
+        {
+            if (!context.ControlIsActuated())
+            {
+                lastDeltaTime = activationTime = 0f;
+                context.Canceled();
+            }
+            else if (context.phase.IsInProgress())
+            {
+                activationTime += lastDeltaTime;
+                context.PerformedAndStayPerformed();
+                context.SetTimeout(0.000001f);
+                lastDeltaTime = Time.deltaTime;
+            }
+            else
+            {
+                context.Started();
+                Process(ref context);
+            }
+        }
+
+        public void Reset()
+        {
+        }
+    }
+}
