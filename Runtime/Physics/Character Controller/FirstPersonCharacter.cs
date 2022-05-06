17c146b456d68ad8399dae42721101fd871eaf17
Casey <hofland.casey@gmail.com>
2022-05-06 10:15:45 +0200

    Improve FirstPersonCharacter inspector
    Input properties are now contained within their own collapsible menu.

--- Runtime/Physics/Character Controller/FirstPersonCharacter.cs
+++ Runtime/Physics/Character Controller/FirstPersonCharacter.cs
@@ -1,4 +1,5 @@
 ﻿#nullable enable
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -19,14 +20,66 @@ namespace UnityExtras
         [field: SerializeField] [field: Tooltip("How far in degrees can you move the camera up")] [field: Min(0f)] public float topClamp { get; set; } = 89.9f;
         [field: SerializeField] [field: Tooltip("How far in degrees can you move the camera down")] [field: Min(0f)] public float bottomClamp { get; set; } = 89.9f;
 
-        [field: Header("Input")]
-        [field: SerializeField] public bool enableInputOnStart { get; set; } = true;
-        [field: SerializeField] public InputActionProperty moveAction { get; set; }
-        [field: SerializeField] public InputActionProperty lookAction { get; set; }
-        [field: SerializeField] public InputActionProperty sprintAction { get; set; }
-        [field: SerializeField] public InputActionProperty jumpAction { get; set; }
-
         private float _lookPitch;
+
+        public void Look(Vector2 movement)
+        {
+            var turnFactor = movement.x;
+            characterMover.Turn(turnFactor);
+
+            var lookFactor = movement.y;
+            if (lookTransform != null)
+            {
+                _lookPitch += lookFactor * characterMover.rotationSpeed * Time.deltaTime;
+                _lookPitch = Mathf.Clamp(_lookPitch, -bottomClamp, topClamp);
+                lookTransform.rotation = transform.rotation * Quaternion.Euler(_lookPitch, 0f, 0f);
+            }
+        }
+
+        #region Input
+        [field: Space(12)]
+        [field: SerializeField] public Input input { get; set; } = new Input();
+
+        public bool enableInputOnStart
+        {
+            get => input.enableInputOnStart;
+            set => input.enableInputOnStart = value;
+        }
+
+        public InputActionProperty moveAction
+        {
+            get => input.moveAction;
+            set => input.moveAction = value;
+        }
+
+        public InputActionProperty lookAction
+        {
+            get => input.lookAction;
+            set => input.lookAction = value;
+        }
+
+        public InputActionProperty sprintAction
+        {
+            get => input.sprintAction;
+            set => input.sprintAction = value;
+        }
+
+        public InputActionProperty jumpAction
+        {
+            get => input.jumpAction;
+            set => input.jumpAction = value;
+        }
+
+        [Serializable]
+        public class Input
+        {
+            [field: SerializeField] public bool enableInputOnStart { get; set; } = true;
+            [field: SerializeField] public InputActionProperty moveAction { get; set; }
+            [field: SerializeField] public InputActionProperty lookAction { get; set; }
+            [field: SerializeField] public InputActionProperty sprintAction { get; set; }
+            [field: SerializeField] public InputActionProperty jumpAction { get; set; }
+        }
+
 
         private void Start()
         {
@@ -69,19 +122,6 @@ namespace UnityExtras
         {
             characterMover.Jump();
         }
-
-        public void Look(Vector2 movement)
-        {
-            var turnFactor = movement.x;
-            characterMover.Turn(turnFactor);
-
-            var lookFactor = movement.y;
-            if (lookTransform != null)
-            {
-                _lookPitch += lookFactor * characterMover.rotationSpeed * Time.deltaTime;
-                _lookPitch = Mathf.Clamp(_lookPitch, -bottomClamp, topClamp);
-                lookTransform.rotation = transform.rotation * Quaternion.Euler(_lookPitch, 0f, 0f);
-            }
-        }
+        #endregion
     }
 }
