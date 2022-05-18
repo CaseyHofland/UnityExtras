0a0069e192675eb0cd3ab013fee6e7a49a4a4285
CaseyDeCoder <hofland.casey@gmail.com>
2022-05-18 15:15:29 +0200

    Change certain method names in ExtraMath
    - Vector2ToRadian has been removed.
    - Vector2ToDegree has been renamed "Angle".
    - RadianToVector2 has been removed.
    - DegreeToVector2 has been renamed "Direction".
    - Rotate2D has been added with the purpose of rotation a vector2 by an amount of degrees.
    
    All the above methods have been given a check to see if they need to perform any sort of calculation, or if they can simply return a default value.

--- Runtime/ExtraMath.cs
+++ Runtime/ExtraMath.cs
@@ -2,6 +2,8 @@
 using UnityEngine;
 using Unity.Mathematics;
 
+using static UnityEngine.Mathf;
+
 namespace UnityExtras
 {
     public class ExtraMath
@@ -11,14 +13,22 @@ namespace UnityExtras
         public static float3 InverseSafe(float3 x, float3 defaultValue = default) => new float3(InverseSafe(x.x, defaultValue.x), InverseSafe(x.yz, defaultValue.yz));
         public static float4 InverseSafe(float4 x, float4 defaultValue = default) => new float4(InverseSafe(x.x, defaultValue.x), InverseSafe(x.yzw, defaultValue.yzw));
 
-        public static float Vector2ToRadian(Vector2 vector2) => Vector2ToDegree(vector2) * Mathf.Deg2Rad;
-        public static float Vector2ToDegree(Vector2 vector2) => Vector2.SignedAngle(Vector2.right, vector2);
+        public static float Angle(Vector2 direction) => direction != Vector2.right ? Vector2.SignedAngle(Vector2.right, direction) : 0f;
+        public static Vector2 Direction(float degrees)
+        {
+            if (degrees == 0f)
+            {
+                return Vector2.right;
+            }
 
-        public static Vector2 RadianToVector2(float radian) => new Vector2(Mathf.Cos(radian), Mathf.Sin(radian));
-        public static Vector2 DegreeToVector2(float degree) => RadianToVector2(degree * Mathf.Deg2Rad);
+            var radians = degrees * Deg2Rad;
+            return new Vector2(Cos(radians), Sin(radians));
+
+        }
+        public static Vector2 Rotate2D(Vector2 x, float degrees) => degrees != 0f ? Direction(Angle(x.normalized) + degrees) * x.magnitude : x;
 
         public static Vector3 JumpVelocity(float desiredHeight) => JumpVelocity(desiredHeight, Physics.gravity);
         public static Vector3 JumpVelocity(float desiredHeight, Vector3 gravity) => JumpVelocity(desiredHeight, gravity.normalized, gravity.magnitude);
-        public static Vector3 JumpVelocity(float desiredHeight, Vector3 gravityDirection, float gravityMagnitude) => -gravityDirection * Mathf.Sqrt(desiredHeight * 2f * gravityMagnitude);
+        public static Vector3 JumpVelocity(float desiredHeight, Vector3 gravityDirection, float gravityMagnitude) => -gravityDirection * Sqrt(desiredHeight * 2f * gravityMagnitude);
     }
 }
