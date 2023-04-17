34db223522db1593db8a166aaa233ed5cdf15aa1
CaseyDeCoder <hofland.casey@gmail.com>
2023-04-17 14:55:35 +0200

    Update Direction Implementation
    Added a RelativeDirection that can take a transform in order to calculate a relative direction. This means you can specify Vector3.down based on the rotation of a transform, which may help out with relative directions.
    
    The old drawer was broken due to a quirk with the inspectors being drawn using UIElements. The drawer has been updated to use UIElements now.
    
    IKPlanter will now ignore any attached rigidbodies and colliders that are children of the rigbuilder, if it can find any.

--- Runtime/Serializable Values/Direction.cs
+++ Runtime/Serializable Values/Direction.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 namespace UnityExtras
 {
     [Serializable]
-    public record Direction
+    public struct Direction : IEquatable<Direction>
     {
-        [SerializeField] private Vector3 _value = Vector3.forward;
+        [SerializeField] private Vector3 _value;
         public Vector3 value
         {
             get => _value;
-            set => _value = value.magnitude > Vector3.kEpsilon ? value.normalized : Vector3.forward;
+            set => _value = value.sqrMagnitude > Vector3.kEpsilonNormalSqrt ? value.normalized : Vector3.forward;
         }
 
         public Quaternion rotation
@@ -23,14 +23,20 @@ namespace UnityExtras
 
         public override string ToString() => value.ToString();
 
+        public override bool Equals(object? obj) => obj is Direction direction && Equals(direction);
+        public bool Equals(Direction other) => _value == other._value;
+        public override int GetHashCode() => _value.GetHashCode();
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Direction operator -(Direction direction) => new() { value = -direction.value };
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator ==(Direction lhs, Direction rhs) => lhs.Equals(rhs);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator !=(Direction lhs, Direction rhs) => !(lhs == rhs);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static implicit operator Vector3(Direction direction) => direction.value;
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static implicit operator Direction(Vector3 value) => new() { value = value };
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector3 operator *(Direction a, float d) => a.value * d;
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector3 operator *(float d, Direction a) => d * a.value;
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector3 operator /(Direction a, float d) => a.value / d;
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator ==(Direction lhs, Vector3 rhs) => lhs.value == rhs;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator ==(Direction lhs, Vector3 rhs) => lhs.value.Equals(rhs);
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator !=(Direction lhs, Vector3 rhs) => !(lhs == rhs);
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static implicit operator Quaternion(Direction direction) => direction.rotation;
@@ -38,7 +44,7 @@ namespace UnityExtras
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Quaternion operator *(Direction direction, Quaternion rotation) => direction.rotation * rotation;
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Quaternion operator *(Quaternion rotation, Direction direction) => rotation * direction.rotation;
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static Vector3 operator *(Direction direction, Vector3 point) => direction.rotation * point;
-        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator ==(Direction lhs, Quaternion rhs) => lhs.rotation == rhs;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator ==(Direction lhs, Quaternion rhs) => lhs.rotation.Equals(rhs);
         [MethodImpl(MethodImplOptions.AggressiveInlining)] public static bool operator !=(Direction lhs, Quaternion rhs) => !(lhs == rhs);
     }
 }
