488f3824bfcfc95a524a3c4525022f6202ec1db9
CaseyDeCoder <hofland.casey@gmail.com>
2023-05-08 23:11:13 +0200

    Fix TurnTowards overshooting.
    TurnTowards would overshoot if the rotation speed was set really high. Now it just snaps to the right direction if this happens.

--- Runtime/Physics/Character Controller/CharacterMover.cs
+++ Runtime/Physics/Character Controller/CharacterMover.cs
@@ -40,17 +40,18 @@ namespace UnityExtras
         public void MoveRelative(Vector3 movement, bool sprint) => Move(transform.rotation * movement, sprint);
 
         /// <include file='./CharacterMover.xml' path='docs/Turn/*'/>
-        public void Turn(float turnFactor) => Turn(Vector3.up, turnFactor);
+        public void Turn(float turnFactor = 1f) => Turn(Vector3.up, turnFactor);
         /// <include file='./CharacterMover.xml' path='docs/Turn/*'/>
-        public void Turn(Vector3 axis, float turnFactor) => transform.Rotate(axis, turnFactor * rotationSpeed * Time.deltaTime, Space.Self);
+        public void Turn(Vector3 axis, float turnFactor = 1f) => transform.Rotate(axis, turnFactor * rotationSpeed * Time.deltaTime, Space.Self);
 
-        public void TurnTowards(Vector3 direction, float errorMargin = 2f)
+        public void TurnTowards(Vector3 direction, float turnFactor = 1f) => TurnTowards(direction, Vector3.up, turnFactor);
+        public void TurnTowards(Vector3 direction, Vector3 axis, float turnFactor = 1f)
         {
             var signedAngle = Vector3.SignedAngle(transform.forward, direction, transform.up);
-            if (Mathf.Abs(signedAngle) > errorMargin)
-            {
-                Turn(Mathf.Sign(signedAngle));
-            }
+            var sign = Mathf.Sign(signedAngle);
+            var angle = Mathf.Abs(signedAngle);
+            angle = Mathf.Min(angle, rotationSpeed * Time.deltaTime);
+            transform.rotation *= Quaternion.AngleAxis(angle * sign * turnFactor, axis);
         }
     }
 }
