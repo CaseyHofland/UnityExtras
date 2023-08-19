ba9bc3634f0633b7c7d8cbec939575f6094a6fef
CaseyHofland <hofland.casey@gmail.com>
2023-08-19 13:15:37 +0200

    Remove commented line from FocusPoint

--- Runtime/Animation/Rigging/FocusPoint.cs
+++ Runtime/Animation/Rigging/FocusPoint.cs
@@ -6,7 +6,6 @@ namespace UnityExtras.Rigging
 {
     public class FocusPoint : MonoBehaviour
     {
-        //[field: SerializeField, Tooltip("The focus points this focus point will alter.")] public FocusPoints? focusPoints { get; set; }
         [field: SerializeField, Tooltip("The priority of this focus point. Higher = More Interesting.")] public int priority { get; set; }
         [field: SerializeField, Header("Trigger Settings"), Tooltip("Should the collider's tag be checked when it enters the trigger?")] public bool checkTag { get; set; }
         [field: SerializeField, Tag, Tooltip("The collision tag the trigger checks for.")] public string? collisionTag { get; set; }
